0c94859d1abc7487d75ecbc3a93ed2615695fe9f
your name <your-email@example.com>
2024-01-14 04:03:47 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :recoverable, :rememberable, :validatable,
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[line]
   def social_profile(provider)
     social_profiles.select { |sp| sp.provider == provider.to_s }.first
