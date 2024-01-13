66c480ec809159b418ce4a8260bd77145e48fb46
your name <your-email@example.com>
2024-01-14 01:36:18 +0900

    ログイン後のページ指定

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -23,7 +23,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
     #ログイン後のflash messageとリダイレクト先を設定
     flash[:notice] = "ログインしました"
-    # redirect_to stopwatches_path
+    redirect_to stopwatches_path
   end
 
   def fake_email(uid, provider)
