150d467cc2edfa1edc6a82af31215d4f3ef0d318
your name <your-email@example.com>
2024-01-18 02:47:02 +0900

    userモデル編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :stopwatches, dependent: :destroy
+  has_many :notification_settings, dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[line]
