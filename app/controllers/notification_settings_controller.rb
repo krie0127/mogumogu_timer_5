3ac2b487724122cca8c28bd69d3ac47c90684def
your name <your-email@example.com>
2024-02-06 23:11:11 +0900

    通知時間の編集

--- app/controllers/notification_settings_controller.rb
+++ app/controllers/notification_settings_controller.rb
@@ -3,7 +3,7 @@
 # LINE通知設定
 class NotificationSettingsController < ApplicationController
   before_action :authenticate_user!
-  # before_action :set_notification_setting, only: [:edit, :update, :destroy]
+  before_action :find_notification_setting, only: [:edit, :update, :destroy]
   
   def index
     @notification_settings = NotificationSetting.order(:preferred_time)
@@ -30,22 +30,24 @@ class NotificationSettingsController < ApplicationController
 
   def update
     if @notification_setting.update(notification_setting_params)
-      # 更新に成功した場合のフラッシュメッセージを設定
-      redirect_to notification_settings_path, success: t('defaults.message.updated', item: Board.model_name.human)
+      redirect_to notification_settings_path, success: '通知が更新されました'
     else
-      # 更新に失敗した場合のフラッシュメッセージを設定
-      flash.now[:danger] = t('defaults.message.not_updated', item: Board.model_name.human)
-      render :edit
+      flash.now[:danger] = ”通知設定の更新に失敗しました”
+      render:edit
     end
   end
 
   def destroy
     @notification_setting = NotificationSetting.find(params[:id])
     @notification_setting.destroy
-    redirect_to notification_settings_path, status: :see_other, notice: '通知が削除されました。'
+    redirect_to notification_settings_path, notice: '通知が削除されました。'
   end
 
   private
+
+  def find_notification_setting
+    @notification_setting = NotificationSetting.find(params[:id])
+  end
 
   def notification_setting_params
     params.require(:notification_setting).permit(:preferred_time)
